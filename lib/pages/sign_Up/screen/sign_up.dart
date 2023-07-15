4e1236d447a8efe9a8ce2f48fe220916048aad07
Rahul <rahulsharma3258@gmail.com>
2023-07-15 20:40:31 +0530

    Sign In & Sign Out Logic Implemented.

--- lib/pages/sign_Up/screen/sign_up.dart
+++ lib/pages/sign_Up/screen/sign_up.dart
@@ -5,10 +5,6 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign Up Screen'),
-      ),
-    );
+    return;
   }
 }
